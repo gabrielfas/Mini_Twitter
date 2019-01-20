3f648e7831d42bb6fe6bb4b65c3a2fc4bbaaba1d
gabrielfas <g.felipes@hotmail.com>
2019-01-20 17:34:48 -0300

    adding a html file

--- Mini_Twitter/tweet/urls.py
+++ Mini_Twitter/tweet/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
-
 from rest_framework import routers
-
 from .views import TweetModelViewSet
+#from django.views.generic import TemplateView
+from .models import Tweet
 
 router = routers.DefaultRouter()
 router.register('', TweetModelViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    #path('todos', TemplateView.as_view(template_name='tweet/index.html'), {'tweets':Tweet.objects.all()}),
 ]
 
